bea6cbf1d61c50c021fbaac9ced0a4ed3aa3696f
van-fs <van@fullstory.com>
2020-06-30 13:44:00 -0400

    update tests to use mocks and mock expects

--- test/logger.spec.ts
+++ test/logger.spec.ts
@@ -1,108 +1,85 @@
 import { expect } from 'chai';
 import 'mocha';
 import { Logger, LogLevel, LogEvent } from '../src/utils/logger';
-import { FullStoryAPI, getFS } from '../src/utils/window';
+import { FullStory } from './vendor/fullstory-recording';
+import { expectParams, MockClass } from './mock';
 
-class MockConsole {
-  messages: { [key: string]: string } = {
-    error: '',
-    warn: '',
-    info: '',
-    debug: ''
-  }
+class MockConsole extends MockClass {
 
-  constructor() {
+  error(message: string): void { }
 
-  }
+  warn(message: string): void { }
 
-  error(message: string) {
-    this.messages.error = message;
-  }
+  info(message: string): void { }
 
-  warn(message: string) {
-    this.messages.warn = message;
-  }
-
-  info(message: string) {
-    this.messages.info = message;
-  }
-
-  debug(message: string) {
-    this.messages.debug = message;
-  }
-}
-
-class MockFullStory implements FullStoryAPI {
-
-  events: [string, { [key: string]: any }, string][] = [];
-
-  constructor() {
-
-  }
-
-  event(eventName: string, payload: { [key: string]: any }, source: string) {
-    this.events.push([eventName, payload, source]);
-  }
+  debug(message: string): void { }
 }
 
 describe('logger unit tests', () => {
 
   it('it should create a console logger with warn and error levels by default', () => {
     const mockDatalayer = 'digitalData.user';
-    const mockConsole = new MockConsole();
+    const console = new MockConsole();
     // @ts-ignore
-    globalThis.console = mockConsole;
+    globalThis.console = console;
 
     const logger = Logger.getInstance();
 
     logger.error('Data layer not found', mockDatalayer);
-    logger.warn('Data layer not ready', mockDatalayer);
-    logger.info('Data layer rules loaded', mockDatalayer);
-    logger.debug('Operator output', mockDatalayer);
+    const [ error ] = expectParams(console, 'error');
+    expect(error).to.eq(`Data layer not found (${mockDatalayer})`);
 
-    expect(mockConsole.messages.error).to.eq(`Data layer not found (${mockDatalayer})`);
-    expect(mockConsole.messages.warn).to.eq(`Data layer not ready (${mockDatalayer})`);
-    expect(mockConsole.messages.info).to.be.empty;
-    expect(mockConsole.messages.debug).to.be.empty;
+    logger.warn('Data layer not ready', mockDatalayer);
+    const [ warn ] = expectParams(console, 'warn');
+    expect(warn).to.eq(`Data layer not ready (${mockDatalayer})`);
+
+    logger.info('Data layer rules loaded', mockDatalayer);
+    expect(console.callQueues.info.length).to.eq(0)
+
+    logger.debug('Operator output', mockDatalayer);
+    expect(console.callQueues.debug.length).to.eq(0)
+
   });
 
   it('it should allow setting higher log levels', () => {
-    const mockConsole = new MockConsole();
+    const console = new MockConsole();
     // @ts-ignore
-    globalThis.console = mockConsole;
+    globalThis.console = console;
 
     const logger = Logger.getInstance();
     logger.level = LogLevel.DEBUG;
 
     logger.info('Data layer rules loaded');
-    logger.debug('Operator output');
+    const [ info ] = expectParams(console, 'info');
+    expect(info).to.eq(`Data layer rules loaded`);
 
-    expect(mockConsole.messages.info).to.eq(`Data layer rules loaded`);
-    expect(mockConsole.messages.debug).to.eq(`Operator output`);
+    logger.debug('Operator output');
+    const [ debug ] = expectParams(console, 'debug');
+    expect(debug).to.eq(`Operator output`);
   });
 
   it('it should allow setting a custom appender', () => {
     const mockDatalayer = 'digitalData.user';
 
-    const mockFullStory = new MockFullStory();
-    (globalThis as any).window = { _fs_namespace: 'FS', FS: mockFullStory };
+    const FS = new FullStory();
 
     const logger = Logger.getInstance();
     logger.appender = {
       log(event: LogEvent) {
         const { level: level_int, message: message_str, datalayer: datalayer_str } = event;
-        getFS()!.event('Data Layer Observer', { level_int, message_str, datalayer_str }, 'dataLayerObserver');
+        FS.event('Data Layer Observer', { level_int, message_str, datalayer_str }, 'dataLayerObserver');
       }
     };
 
     logger.error('Data layer not found', mockDatalayer);
 
-    const event = mockFullStory.events[0];
-    expect(event[0]).to.eq('Data Layer Observer');
-    expect(event[1].level_int).to.eq(LogLevel.ERROR);
-    expect(event[1].message_str).to.eq('Data layer not found');
-    expect(event[1].datalayer_str).to.eq(mockDatalayer);
-    expect(event[2]).to.eq('dataLayerObserver');
+    const [eventName, payload, source] = expectParams(FS, 'event');
+    expect(eventName).to.eq('Data Layer Observer');
+    expect(payload.level_int).to.eq(LogLevel.ERROR);
+    expect(payload.message_str).to.eq('Data layer not found');
+    expect(payload.datalayer_str).to.eq(mockDatalayer);
+    expect(source).to.eq('dataLayerObserver');
+
   });
 
 });
--- test/vendor.spec.ts
+++ test/vendor.spec.ts
@@ -2,6 +2,7 @@ import { expect } from 'chai';
 import 'mocha';
 
 import { FullStory } from './vendor/fullstory-recording';
+import { expectParams } from './mock';
 
 describe('mock FullStory recording tests', () => {
 
@@ -10,13 +11,14 @@ describe('mock FullStory recording tests', () => {
 
     expect(FS.callQueues.consent.length).to.eq(0);
     FS.consent(true);
-    expect(FS.callQueues.consent.length).to.eq(1);
-    expect(FS.callQueues.consent.pop().parameters[0]).to.be.true;
+    const [ consented ] = expectParams(FS, 'consent');
+    expect(consented).to.be.true;
 
     expect(FS.callQueues.log.length).to.eq(0);
     FS.log(0, 'value')
-    expect(FS.callQueues.log.length).to.eq(1);
-    expect(FS.callQueues.log.pop().parameters[1]).to.eq('value');
+    const [ logLevel, message ] = expectParams(FS, 'log');
+    expect(logLevel).to.eq(0);
+    expect(message).to.eq('value');
   });
 
 });
