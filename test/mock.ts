bd96facf4130d4fa9b6cba0b5367c897d493f8ef
van-fs <van@fullstory.com>
2020-06-30 13:43:32 -0400

    fix ts compile errors and add convenience expects

--- test/mock.ts
+++ test/mock.ts
@@ -1,4 +1,6 @@
 
+import { expect } from 'chai';
+
 export class MockClass {
 
   public callQueues: { [methodName: string]: Call[] } = {}
@@ -7,8 +9,8 @@ export class MockClass {
     for (const methodName of Object.getOwnPropertyNames(this.constructor.prototype)) {
       if (methodName === 'constructor') continue;
       this.callQueues[methodName] = [];
-      const originalMethod = this[methodName].bind(this);
-      this[methodName] = (...params) => {
+      const originalMethod: Function = (this as any)[methodName].bind(this);
+      (this as any)[methodName] = (...params: any[]) => {
         const result = originalMethod(...params);
         this.callQueues[methodName].push(new Call(
           params,
@@ -21,5 +23,27 @@ export class MockClass {
 }
 
 export class Call {
-  constructor(public parameters: object, public result: string | null | void){}
+  constructor(public parameters: any[], public result: string | null | void){}
+}
+
+/**
+ * Tests whether a call queue has one Call and returns it.
+ * @param mock the MockClass
+ * @param methodName the call queue's method name
+ */
+export function expectCall(mock: MockClass, methodName: string): Call {
+  expect(mock.callQueues[methodName].length).to.eq(1);
+  return mock.callQueues[methodName].pop()!;
+}
+
+/**
+ * Tests whether a call queue has one Call and returns the Call's parameters.
+ * @param mock the MockClass
+ * @param methodName the call queue's method name
+ */
+export function expectParams(mock: MockClass, methodName: string): any[] {
+  const { parameters } = expectCall(mock, methodName);
+  expect(parameters).to.not.be.undefined;
+  expect(parameters).to.not.be.null;
+  return parameters;
 }
