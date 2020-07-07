77fdd98fdb093c3a9f74827f9e3f8420f9a3e074
van-fs <van@fullstory.com>
2020-07-07 10:50:37 -0400

    lint test mock fixes part 2

--- test/mocks/mock.ts
+++ test/mocks/mock.ts
@@ -7,18 +7,19 @@ export class MockClass {
   public callQueues: { [methodName: string]: Call[] } = {}
 
   constructor() {
-    for (const methodName of Object.getOwnPropertyNames(this.constructor.prototype)) {
-      if (methodName === 'constructor') continue;
-      this.callQueues[methodName] = [];
-      const originalMethod: Function = (this as any)[methodName].bind(this);
-      (this as any)[methodName] = (...parameters: any[]) => {
-        const result = originalMethod(...parameters);
-        this.callQueues[methodName].push({
-          parameters,
-          result,
-        });
-        return result;
-      };
-    }
+    Object.getOwnPropertyNames(this.constructor.prototype).forEach((methodName: string) => {
+      if (methodName !== 'constructor') {
+        this.callQueues[methodName] = [];
+        const originalMethod: Function = (this as any)[methodName].bind(this);
+        (this as any)[methodName] = (...parameters: any[]) => {
+          const result = originalMethod(...parameters);
+          this.callQueues[methodName].push({
+            parameters,
+            result,
+          });
+          return result;
+        };
+      }
+    });
   }
 }
