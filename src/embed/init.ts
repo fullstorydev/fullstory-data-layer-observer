d1e2dd012860260815c245aeecb4ce5946653971
Trevor F Smith <trevor@trevor.smith.name>
2020-07-13 09:02:55 -0700

    Added a build for the embedded script and started a snippet for review.

---
+++ src/embed/init.ts
@@ -0,0 +1,30 @@
+import { DataLayerObserver } from '../observer';
+
+/*
+This is where we initialize the DataLayerObserver from this info:
+
+// If true, output from rules are sent only to `console`
+// Default is false
+window['_dlo_preview'] = false;
+
+// If true, rules will run on page load in addition to when there are changes
+// Default is true
+window['_dlo_readOnload'] = false;
+
+// Anything on `window` that starts with `_dlo_rules` is read as a rules array
+window['_dlo_rules'] = [
+// rules can go here
+];
+
+window['_dlo_rulesFromAdTeam'] = [
+// rules can also go here
+];
+
+window['_dlo_rulesFromOpsTeam'] = [
+// rules can also go here
+];
+
+*/
+
+/* eslint no-console: "off" */
+console.log('TBD: Embed init', DataLayerObserver);
