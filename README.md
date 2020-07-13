5942b30ff0e46e1031d34640eb5a734c09d5dd81
Trevor F Smith <trevor@trevor.smith.name>
2020-07-13 11:36:08 -0700

    The embed script now initializes the DLO based on window values like _dlo_preview, _dlo_readOnLoad, and _dlo_rules*. Added a way to run samples in the examples dir.

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # fullstory-data-layer-observer
 An API for recording analytics data layer state into FullStory
+
+## Examples
+
+The `fullstory-data-layer-observer/examples/` folder holds a variety of samples of the Data Layer Observer in action.
+
+To run these examples:
+
+	cd fullstory-data-layer-observer/
+	npm install # you only need to do this once
+	npm run examples
+
+Now point your browser at http://127.0.0.1:8080/examples/
+
--- src/embed/init.ts
+++ src/embed/init.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-underscore-dangle, camelcase */
 import { DataLayerObserver } from '../observer';
 
 /*
@@ -9,7 +10,7 @@ window['_dlo_preview'] = false;
 
 // If true, rules will run on page load in addition to when there are changes
 // Default is true
-window['_dlo_readOnload'] = false;
+window['_dlo_readOnLoad'] = false;
 
 // Anything on `window` that starts with `_dlo_rules` is read as a rules array
 window['_dlo_rules'] = [
@@ -26,5 +27,50 @@ window['_dlo_rulesFromOpsTeam'] = [
 
 */
 
-/* eslint no-console: "off" */
-console.log('TBD: Embed init', DataLayerObserver);
+function _dlo_collectRules(): any[] {
+  const results: any[] = [];
+  Object.getOwnPropertyNames(window).forEach((propName) => {
+    if (propName.startsWith('_dlo_rules') === false) return;
+
+    const prop = (window as { [key: string]: any })[propName];
+    if (Array.isArray(prop) === false) {
+      /* eslint-disable no-console */
+      console.warn(`window[${propName}] is not an array so will be ignored`);
+      return;
+    }
+
+    prop.forEach((rule: any) => {
+      results.push(rule);
+    });
+  });
+  return results;
+}
+
+function _dlo_initializeFromWindow() {
+  const win = (window as { [key: string]: any });
+
+  // Read flags
+  let preview = false;
+  if (win._dlo_preview === true) {
+    preview = true;
+  }
+  let readOnLoad = false;
+  if (win._dlo_readOnLoad === true) {
+    readOnLoad = true;
+  }
+
+  // Read rules
+  const rules = _dlo_collectRules();
+  if (rules.length === 0) {
+    /* eslint-disable no-console */
+    console.warn('No rules for the Data Layer Observer');
+  }
+
+  win._dlo_observer = new DataLayerObserver({
+    previewMode: preview,
+    readOnLoad,
+    rules,
+  });
+}
+
+_dlo_initializeFromWindow();
