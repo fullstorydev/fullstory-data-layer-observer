da637a8e1f91dfe3b7bc6f90a98ecda9f127d9d9
Van Staub <52075243+van-fs@users.noreply.github.com>
2020-07-14 13:44:25 -0400

    Add flatten operator (#31)
    * add flatten operator

---
+++ src/operators/flatten.ts
@@ -0,0 +1,62 @@
+import { Operator, OperatorOptions, OperatorValidator } from '../operator';
+
+export interface FlattenOperatorOptions extends OperatorOptions {
+
+}
+
+export class FlattenOperator implements Operator {
+  static specification = {
+    index: { required: false, type: ['number'] },
+    maxDepth: { required: false, type: ['number'] },
+  };
+
+  readonly index: number;
+
+  readonly maxDepth: number;
+
+  constructor(public options: FlattenOperatorOptions) {
+    const { index = 0, maxDepth = 10 } = options;
+
+    this.index = index;
+    this.maxDepth = maxDepth;
+  }
+
+  /**
+   * Recursively flattens all properties into an object.
+   * @param root the root object to contain all properties
+   * @param node a child node to flatten
+   */
+  flattenHelper(root: any, depth = 0, node?: any) {
+    let targetNode = node;
+    let targetRoot = root;
+
+    if (!targetNode) {
+      // first time execution
+      targetNode = root;
+      targetRoot = {};
+    }
+
+    Object.getOwnPropertyNames(targetNode).forEach((prop) => {
+      if (typeof targetNode[prop] === 'object' && targetNode[prop] != null
+        && !Array.isArray(targetNode[prop]) && depth < this.maxDepth + 1) {
+        this.flattenHelper(targetRoot, depth + 1, targetNode[prop]);
+      } else {
+        targetRoot[prop] = targetNode[prop];
+      }
+    });
+
+    return targetRoot;
+  }
+
+  handleData(data: any[]): any[] | null {
+    const flattenedData = data;
+    flattenedData[this.index] = this.flattenHelper(flattenedData[this.index]);
+
+    return flattenedData;
+  }
+
+  validate() {
+    const validator = new OperatorValidator(this.options);
+    validator.validate(FlattenOperator.specification);
+  }
+}
---
+++ test/operator-flatten.spec.ts
@@ -0,0 +1,83 @@
+import { expect } from 'chai';
+import 'mocha';
+
+import { FlattenOperator } from '../src/operators';
+import { basicDigitalData } from './mocks/CEDDL';
+
+describe('flatten operator unit tests', () => {
+  it('it should validate options', () => {
+    expect(() => new FlattenOperator({
+      name: 'flatten',
+    }).validate()).to.not.throw();
+
+    expect(() => new FlattenOperator({
+      name: 'flatten', index: 1,
+    }).validate()).to.not.throw();
+
+    expect(() => new FlattenOperator({
+      name: 'flatten', maxDepth: 3,
+    }).validate()).to.not.throw();
+  });
+
+  it('it should flatten an object', () => {
+    const userProfile = basicDigitalData.user.profile[0];
+    const operator = new FlattenOperator({ name: 'flatten' });
+    const [flatUser] = operator.handleData([userProfile])!;
+
+    expect(flatUser).to.not.be.null;
+    expect(flatUser!.profileID).to.eq(userProfile.profileInfo.profileID);
+    expect(flatUser!.userName).to.eq(userProfile.profileInfo.userName);
+    expect(flatUser!.city).to.eq(userProfile.address.city);
+    expect(flatUser!.stateProvince).to.eq(userProfile.address.stateProvince);
+  });
+
+  it('it should not flatten array contents', () => {
+    const userProfile = {
+      nicknames: ['Jon', 'Johny'],
+      ...basicDigitalData.user.profile[0],
+    };
+    const operator = new FlattenOperator({ name: 'flatten' });
+    const [flatUser] = operator.handleData([userProfile])!;
+
+    expect(flatUser).to.not.be.null;
+    expect(flatUser!.nicknames).to.eq(userProfile.nicknames);
+  });
+
+  it('it should flatten an object at a specific index', () => {
+    const userProfile = basicDigitalData.user.profile[0];
+    const operator = new FlattenOperator({ name: 'flatten', index: 1 });
+    const [profileID, flatUser] = operator.handleData([userProfile.profileInfo.profileID, userProfile])!;
+
+    expect(flatUser).to.not.be.null;
+    expect(profileID).to.not.be.null;
+    expect(flatUser!.profileID).to.eq(userProfile.profileInfo.profileID);
+    expect(flatUser!.userName).to.eq(userProfile.profileInfo.userName);
+    expect(flatUser!.city).to.eq(userProfile.address.city);
+    expect(flatUser!.stateProvince).to.eq(userProfile.address.stateProvince);
+  });
+
+  it('it should flatten an object to a specific depth', () => {
+    const userProfile = {
+      nicknames: ['Jon', 'Johny'],
+      children: {
+        junior: {
+          name: 'Jon Jr.',
+        },
+        tripp: {
+          name: 'Jon III',
+        },
+      },
+      ...basicDigitalData.user.profile[0],
+    };
+    const operator = new FlattenOperator({ name: 'flatten', maxDepth: 1 });
+    const [flatUser] = operator.handleData([userProfile])!;
+
+    expect(flatUser).to.not.be.null;
+    expect(flatUser!.profileID).to.eq(userProfile.profileInfo.profileID);
+    expect(flatUser!.userName).to.eq(userProfile.profileInfo.userName);
+    expect(flatUser!.city).to.eq(userProfile.address.city);
+    expect(flatUser!.stateProvince).to.eq(userProfile.address.stateProvince);
+    expect(flatUser!.junior).to.be.undefined;
+    expect(flatUser!.tripp).to.be.undefined;
+  });
+});
