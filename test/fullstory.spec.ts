d4c1fb9dd6e91ce16667da6e84333b932a67876a
Van Staub <52075243+van-fs@users.noreply.github.com>
2020-07-20 13:28:46 -0400

    add FullStory CEDDL Cart spec (#38)
    * add cart spec
    
    * remove custom properties

--- test/fullstory.spec.ts
+++ test/fullstory.spec.ts
@@ -1,8 +1,9 @@
 import { expect } from 'chai';
 import 'mocha';
 
-import { DataLayerObserver } from '../src/observer';
-import * as rules from '../examples/rules/ceddl-user-fullstory.json';
+import { DataLayerObserver, DataLayerRule } from '../src/observer';
+import * as userRules from '../examples/rules/ceddl-user-fullstory.json';
+import * as cartRules from '../examples/rules/ceddl-cart-fullstory.json';
 
 import { CEDDL, basicDigitalData } from './mocks/CEDDL';
 import Console from './mocks/console';
@@ -16,6 +17,15 @@ interface GlobalMock {
 }
 
 let globalMock: GlobalMock;
+
+const rules = [...cartRules.rules, ...userRules.rules];
+
+function getRule(id: string) {
+  const rule = rules.find((r: DataLayerRule) => r.id === id);
+  expect(rule).to.not.be.undefined;
+
+  return rule!;
+}
 
 describe('FullStory example rules unit tests', () => {
   beforeEach(() => {
@@ -34,7 +44,7 @@ describe('FullStory example rules unit tests', () => {
 
     (globalThis as any).digitalData.user.profile[0].job = 'developer'; // inject custom property
 
-    const observer = new DataLayerObserver({ rules: [rules[0]], readOnLoad: true });
+    const observer = new DataLayerObserver({ rules: [getRule('fs-uservars-ceddl-user-all')], readOnLoad: true });
     expect(observer).to.not.be.undefined;
 
     const [payload] = expectParams(globalMock.FS, 'setUserVars');
@@ -51,13 +61,13 @@ describe('FullStory example rules unit tests', () => {
     expect(payload.attributes).to.be.undefined;
     expect(payload.job).to.eq('developer'); // verify custom property
 
-    (globalThis as any).digitalData.user.profile[0].job; // remove custom property
+    delete (globalThis as any).digitalData.user.profile[0].job; // remove custom property
   });
 
   it('it should send any CEDDL user property to FS.identify', () => {
     const { profileInfo, address } = basicDigitalData.user.profile[0];
 
-    const observer = new DataLayerObserver({ rules: [rules[1]], readOnLoad: true });
+    const observer = new DataLayerObserver({ rules: [getRule('fs-identify-ceddl-user-all')], readOnLoad: true });
     expect(observer).to.not.be.undefined;
 
     const [uid, payload] = expectParams(globalMock.FS, 'identify');
@@ -72,7 +82,7 @@ describe('FullStory example rules unit tests', () => {
 
     const { profileInfo, address } = basicDigitalData.user.profile[0];
 
-    const observer = new DataLayerObserver({ rules: [rules[2]], readOnLoad: true });
+    const observer = new DataLayerObserver({ rules: [getRule('fs-identify-ceddl-user-allowed')], readOnLoad: true });
     expect(observer).to.not.be.undefined;
 
     const [uid, payload] = expectParams(globalMock.FS, 'identify');
@@ -82,5 +92,71 @@ describe('FullStory example rules unit tests', () => {
     expect(payload.password).to.be.undefined;
 
     delete (globalThis as any).digitalData.user.profile[0].password; // remove sensitive property
+  });
+
+  it('it should send CEDDL cart cartID and price properties to FS.event', () => {
+    const { cartID, price } = basicDigitalData.cart;
+
+    (globalThis as any).digitalData.cart.promotion = 'LaborDay2020'; // inject custom property
+
+    const observer = new DataLayerObserver({ rules: [getRule('fs-event-ceddl-cart')], readOnLoad: true });
+    expect(observer).to.not.be.undefined;
+
+    const [eventName, payload] = expectParams(globalMock.FS, 'event');
+    expect(eventName).to.eq('View Cart');
+    expect(payload.cartID).to.eq(cartID);
+    expect(payload.price).to.eq(price);
+    expect(payload.promotion).to.be.undefined;
+
+    delete (globalThis as any).digitalData.cart.promotion; // remove custom property
+  });
+
+  it('it should send all CEDDL cart properties except items to FS.event', () => {
+    // NOTE that items is a list of complex objects that requires special transformations (see FS.event limitations)
+    const { cartID, price, attributes } = basicDigitalData.cart;
+
+    (globalThis as any).digitalData.cart.promotion = 'LaborDay2020'; // inject custom property
+
+    const observer = new DataLayerObserver({ rules: [getRule('fs-event-ceddl-cart-not-items')], readOnLoad: true });
+    expect(observer).to.not.be.undefined;
+
+    const [eventName, payload] = expectParams(globalMock.FS, 'event');
+    expect(eventName).to.eq('View Cart');
+    expect(payload.cartID).to.eq(cartID);
+    expect(payload.price).to.eq(price);
+    expect(payload.attributes).to.eq(attributes);
+    expect(payload.promotion).to.eq('LaborDay2020');
+    expect(payload.items).to.be.undefined;
+
+    delete (globalThis as any).digitalData.cart.promotion; // remove custom property
+  });
+
+  it('it should convert strings to reals and send CEDDL cart properties to FS.event', () => {
+    const { price: { basePrice, priceWithTax, cartTotal } } = basicDigitalData.cart;
+
+    // convert to strings for testing
+    (globalThis as any).digitalData.cart.price.basePrice = basePrice.toString();
+    (globalThis as any).digitalData.cart.price.priceWithTax = priceWithTax.toString();
+    (globalThis as any).digitalData.cart.price.cartTotal = cartTotal.toString();
+
+    expect(typeof (globalThis as any).digitalData.cart.price.basePrice).to.eq('string');
+    expect(typeof (globalThis as any).digitalData.cart.price.priceWithTax).to.eq('string');
+    expect(typeof (globalThis as any).digitalData.cart.price.cartTotal).to.eq('string');
+
+    const observer = new DataLayerObserver({ rules: [getRule('fs-event-ceddl-cart-convert')], readOnLoad: true });
+    expect(observer).to.not.be.undefined;
+
+    const [eventName, payload] = expectParams(globalMock.FS, 'event');
+    expect(eventName).to.eq('View Cart');
+
+    // NOTE these are flattened but you could also simply send digitalData.cart.price
+    expect(payload.basePrice).to.eq(basePrice);
+    expect(payload.priceWithTax).to.eq(priceWithTax);
+    expect(payload.cartTotal).to.eq(cartTotal);
+
+    // reset to original values
+    (globalThis as any).digitalData.cart.price.basePrice = basePrice;
+    (globalThis as any).digitalData.cart.price.priceWithTax = priceWithTax;
+    (globalThis as any).digitalData.cart.price.cartTotal = cartTotal;
   });
 });
