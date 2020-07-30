86754a8349d017d26a9644aac92e21bcc27ab0b9
Trevor F Smith <trevor@trevor.smith.name>
2020-07-30 09:41:15 -0700

    Re-introduce the fixes for WEL formulation and testing. Fix one bug in the convert operator validation function.

--- test/web-embed-lab/auto-formulate.sh
+++ test/web-embed-lab/auto-formulate.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+killall ngrok
 set -e
+
+ngrok http -config=./ngrok-auto-formulate-config.yml -subdomain=dlowel 8080 &
 
 rm -rf ./captures/*
 rm -rf ./page-formulas/*
 FRONT_END_DIST=../../node_modules/web-embed-lab/static/ auto-formulate ./auto-formulate.conf.json ./page-formulas/
+
+set +e
+killall ngrok
--- test/web-embed-lab/run-tests.sh
+++ test/web-embed-lab/run-tests.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+killall ngrok
 set -e
 
 FRONT_END_DIST=../../node_modules/web-embed-lab/static/ runner ./page-formulas/ ./test-probes/ ../../build/dlo.js ./runner.conf.json
+
+set +e
+killall ngrok
