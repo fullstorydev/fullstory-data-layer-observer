9a84306ce72cf0b0d4ebce132c1a1843dc967386
Trevor F Smith <trevor@trevor.smith.name>
2020-08-31 09:37:28 -0700

    The fan out operator properties parameter is now optional.

--- src/operators/fan-out.ts
+++ src/operators/fan-out.ts
@@ -7,7 +7,7 @@ export interface FanOutOperatorOptions extends OperatorOptions {
 export class FanOutOperator implements Operator {
   static specification = {
     index: { required: false, type: ['number'] },
-    properties: { required: true, type: ['string'] },
+    properties: { required: false, type: ['string'] },
   };
 
   readonly index: number;
@@ -26,16 +26,41 @@ export class FanOutOperator implements Operator {
 
   /*
   The FanOutOperator is a special case.
+
   `handleData` returns an array of objects that each should have the remaining operators run on them.
+
   If a property value is an array then each item in the array is fanned out.
+
+  If there are no declared properties then:
+  - if it's an array then return all items
+  - else return all 'own' properties that are objects
   */
   handleData(data: any[]): any[] | null {
-    if (typeof data[this.index] !== 'object') {
-      throw new Error('Can only fan out property names on objects');
+    const datum = data[this.index];
+    if (typeof datum !== 'object' && Array.isArray(datum) === false) {
+      throw new Error('Can only fan out arrays or properties on objects');
     }
     const plucked: any[] = [];
+
+    // If properties is empty then work on the datum itself
+    if (this.properties.length === 0) {
+      if (Array.isArray(datum)) {
+        plucked.push(...datum);
+      } else {
+        Object.values(datum).forEach((value: any) => {
+          if (Array.isArray(value)) {
+            plucked.push(...value);
+          } else if (typeof value === 'object') {
+            plucked.push(value);
+          }
+          // We don't pluck non-object and non-array properties
+        });
+      }
+      return plucked;
+    }
+
     for (let i = 0; i < this.properties.length; i += 1) {
-      const value = data[this.index][this.properties[i]];
+      const value = datum[this.properties[i]];
       if (Array.isArray(value)) {
         plucked.push(...value);
       } else {
