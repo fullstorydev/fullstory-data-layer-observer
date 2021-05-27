2a186dccf635e6d940992db0fd13fe37c74b214d
van-fs <van@fullstory.com>
2021-05-27 08:17:52 -0400

    add google rulesets

---
+++ rulesets/README.md
@@ -0,0 +1,17 @@
+# FullStory Data Layer Observer Rulesets
+
+Rulesets provide out of the box rules that are compatible with common data layers and analytics vendors. Where possible, rules follow vendor guidelines and have associated test cases to ensure operation.
+
+## Google
+
+### Event Measurement
+
+- [Ruleset](./google-event-measurement.json)
+- [Vendor Guidelines](https://developers.google.com/analytics/devguides/collection/analyticsjs/events)
+- [Tests](../test/rules-google-fullstory.spec)
+
+### Enhanced Ecommerce (UA)
+
+- [Ruleset](./google-ua-enhanced-ecommerce.json)
+- [Vendor Guidelines](https://developers.google.com/tag-manager/enhanced-ecommerce)
+- [Tests](../test/rules-google-fullstory.spec)
--- test/rules-google-fullstory.spec.ts
+++ test/rules-google-fullstory.spec.ts
@@ -1,16 +1,84 @@
+import { expect } from 'chai';
 import 'mocha';
 
-import { rules } from '../examples/rules/google-tags-fullstory.json';
+import * as EnhancedEcommerce from '../rulesets/google-ua-enhanced-ecommerce.json';
+import * as EventMeasurment from '../rulesets/google-event-measurement.json';
 
-import { basicGoogleTags } from './mocks/google-tags';
 import {
-  expectEqual, expectRule, expectFS, setupGlobals, ExpectObserver, expectGlobal, expectUndefined,
+  expectEqual, expectRule, expectFS, setupGlobals, ExpectObserver, expectGlobal, expectNoCalls, expectCall,
 } from './utils/mocha';
 
-describe('Google Tags to FullStory rules', () => {
+describe('Google Analytics Event Measurement rules', () => {
   beforeEach(() => setupGlobals([
-    ['dataLayer', basicGoogleTags],
-    ['_dlo_rules', rules],
+    ['dataLayer', []],
+    ['_dlo_rules', EventMeasurment.rules],
+  ]));
+
+  afterEach(() => {
+    ExpectObserver.getInstance().cleanup();
+  });
+
+  it('sends an object-based event to FS.event with the same event name', () => {
+    ExpectObserver.getInstance().create({
+      rules: [expectRule('fs-ga-event')],
+    });
+
+    // NOTE that this follows the object-based convention seen in most GA events
+    expectGlobal('dataLayer').push({ event: 'helloWord' });
+    const [eventName] = expectFS('event');
+    expectEqual(eventName, 'helloWord');
+  });
+
+  it('sends an list-based event to FS.event with the same event name', () => {
+    ExpectObserver.getInstance().create({
+      rules: [expectRule('fs-gtg-event')],
+    });
+
+    // NOTE that this follows the list-based convention seen in later versions of gtg.js
+    expectGlobal('dataLayer').push(['event', 'screen_view', { firebase_screen_class: 'app-products' }]);
+    const [eventName, payload] = expectFS('event');
+    expectEqual(eventName, 'screen_view');
+    expect(payload).to.not.be.undefined;
+    expectEqual(payload.firebase_screen_class, 'app-products');
+  });
+
+  it('ignores gtm, optimize.domChange, and enhanced ecommerce related events', () => {
+    ExpectObserver.getInstance().create({
+      rules: [expectRule('fs-ga-event')],
+    });
+
+    // verify the call queue expect code is working
+    expectGlobal('dataLayer').push({ event: 'helloWord' });
+    expectCall(expectGlobal('FS'), 'event');
+
+    // now check that no calls get queued
+
+    // object-based
+    expectGlobal('dataLayer').push({ event: 'checkout', ecommerce: { total: 99.99 } });
+    expectNoCalls(expectGlobal('FS'), 'event');
+
+    expectGlobal('dataLayer').push({ event: 'gtm.click' });
+    expectNoCalls(expectGlobal('FS'), 'event');
+
+    expectGlobal('dataLayer').push({ event: 'optimize.domChange' });
+    expectNoCalls(expectGlobal('FS'), 'event');
+
+    // list-based
+    expectGlobal('dataLayer').push(['event', 'checkout', { ecommerce: { total: 99.99 } }]);
+    expectNoCalls(expectGlobal('FS'), 'event');
+
+    expectGlobal('dataLayer').push(['event', 'gtm.click', {}]);
+    expectNoCalls(expectGlobal('FS'), 'event');
+
+    expectGlobal('dataLayer').push(['event', 'optimize.domChange', {}]);
+    expectNoCalls(expectGlobal('FS'), 'event');
+  });
+});
+
+describe('Google Analytics Enhanced Ecommerce rules', () => {
+  beforeEach(() => setupGlobals([
+    ['dataLayer', []],
+    ['_dlo_rules', EnhancedEcommerce.rules],
   ]));
 
   afterEach(() => {
@@ -19,29 +87,47 @@ describe('Google Tags to FullStory rules', () => {
 
   it('should read pageview', () => {
     ExpectObserver.getInstance().create({
-      rules: [expectRule('fs-ga-pageview')],
+      rules: [
+        expectRule('fs-ga-pageview'),
+      ],
     });
-
-    let [eventName, payload] = expectFS('event');
-    expectEqual(eventName, 'pageview');
-    expectEqual(payload.pageType, 'Home');
 
     expectGlobal('dataLayer').push({
-      pageType: 'Test',
-      pageName: 'test',
+      pageType: 'Home',
+      pageName: 'Home: Fruit shoppe',
     });
 
-    [eventName, payload] = expectFS('event');
+    // TODO (van) change rule to setVars API after go live
+    const [eventName, payload] = expectFS('event');
     expectEqual(eventName, 'pageview');
-    expectEqual(payload.pageName, 'test');
+    expectEqual(payload.pageType, 'Home');
+    expectEqual(payload.pageName, 'Home: Fruit shoppe');
   });
 
   it('should read enhanced ecommerce detail', () => {
     ExpectObserver.getInstance().create({
       rules: [
         expectRule('fs-ga-e-commerce-detail-product'),
-        { ...expectRule('fs-ga-e-commerce-detail-action'), source: 'dataLayer[5]' },
+        expectRule('fs-ga-e-commerce-detail-action'),
       ],
+    });
+
+    expectGlobal('dataLayer').push({
+      ecommerce: {
+        detail: {
+          actionField: { action: 'detail', list: 'Product Gallery' },
+          products: [
+            {
+              name: 'Heritage Huckleberries',
+              id: 'P000525722',
+              price: '2.99',
+              brand: 'Heritage',
+              category: 'product gallery',
+              variant: '',
+            },
+          ],
+        },
+      },
     });
 
     let [eventName, payload] = expectFS('event');
@@ -58,8 +144,28 @@ describe('Google Tags to FullStory rules', () => {
     ExpectObserver.getInstance().create({
       rules: [
         expectRule('fs-ga-e-commerce-click-product'),
-        { ...expectRule('fs-ga-e-commerce-click-action'), source: 'dataLayer[4]' },
+        expectRule('fs-ga-e-commerce-click-action'),
       ],
+    });
+
+    expectGlobal('dataLayer').push({
+      event: 'productClick',
+      ecommerce: {
+        click: {
+          actionField: { action: 'click', list: 'Search Results' },
+          products: [
+            {
+              name: 'Heritage Huckleberries',
+              id: 'P000525722',
+              price: '2.99',
+              brand: 'Heritage',
+              category: 'homepage product recs',
+              variant: '',
+              position: 1,
+            },
+          ],
+        },
+      },
     });
 
     let [eventName, payload] = expectFS('event');
@@ -76,8 +182,29 @@ describe('Google Tags to FullStory rules', () => {
     ExpectObserver.getInstance().create({
       rules: [
         expectRule('fs-ga-e-commerce-add-product'),
-        { ...expectRule('fs-ga-e-commerce-add-action'), source: 'dataLayer[6]' },
+        expectRule('fs-ga-e-commerce-add-action'),
       ],
+    });
+
+    expectGlobal('dataLayer').push({
+      event: 'addToCart',
+      ecommerce: {
+        currencyCode: 'USD',
+        add: {
+          actionField: { action: 'add' },
+          products: [
+            {
+              name: 'Heritage Huckleberries',
+              id: 'P000525722',
+              price: '2.99',
+              brand: 'Heritage',
+              category: 'product',
+              variant: '',
+              quantity: 2,
+            },
+          ],
+        },
+      },
     });
 
     let [eventName, payload] = expectFS('event');
@@ -93,8 +220,29 @@ describe('Google Tags to FullStory rules', () => {
     ExpectObserver.getInstance().create({
       rules: [
         expectRule('fs-ga-e-commerce-remove-product'),
-        { ...expectRule('fs-ga-e-commerce-remove-action'), source: 'dataLayer[7]' },
+        expectRule('fs-ga-e-commerce-remove-action'),
       ],
+    });
+
+    expectGlobal('dataLayer').push({
+      event: 'removeFromCart',
+      ecommerce: {
+        currencyCode: 'USD',
+        remove: {
+          actionField: { action: 'remove' },
+          products: [
+            {
+              name: 'Heritage Huckleberries',
+              id: 'P000525722',
+              price: '2.99',
+              brand: 'Heritage',
+              category: 'product',
+              variant: '',
+              quantity: 1,
+            },
+          ],
+        },
+      },
     });
 
     let [eventName, payload] = expectFS('event');
@@ -110,8 +258,29 @@ describe('Google Tags to FullStory rules', () => {
     ExpectObserver.getInstance().create({
       rules: [
         expectRule('fs-ga-e-commerce-promo_click-promotion'),
-        { ...expectRule('fs-ga-e-commerce-promo_click-action'), source: 'dataLayer[9]' },
+        expectRule('fs-ga-e-commerce-promo_click-action'),
       ],
+    });
+
+    expectGlobal('dataLayer').push({
+      event: 'promotionClick',
+      ecommerce: {
+        promoClick: {
+          actionField: { action: 'promo_click' },
+          promotions: [
+            {
+              id: '1004-Blueberries123321',
+              name: 'Fruits',
+              creative: 'Blueberries123321',
+              position: 'Feature',
+            },
+          ],
+        },
+      },
+      eventCallback() {
+        console.log('Callback called');
+      },
+      'gtm.uniqueEventId': 6,
     });
 
     let [eventName, payload] = expectFS('event');
@@ -126,8 +295,45 @@ describe('Google Tags to FullStory rules', () => {
     ExpectObserver.getInstance().create({
       rules: [
         expectRule('fs-ga-e-commerce-purchase-product'),
-        { ...expectRule('fs-ga-e-commerce-purchase-action'), source: 'dataLayer[11]' },
+        expectRule('fs-ga-e-commerce-purchase-action'),
       ],
+    });
+
+    expectGlobal('dataLayer').push({
+      ecommerce: {
+        purchase: {
+          actionField: {
+            action: 'purchase',
+            id: 'T12345',
+            affiliation: 'Online Store',
+            revenue: '35.43',
+            tax: '4.90',
+            shipping: '5.99',
+            coupon: '',
+          },
+          products: [
+            {
+              name: 'Heritage Huckleberries',
+              id: 'P000525722',
+              price: '2.99',
+              brand: 'Heritage',
+              category: 'fruit',
+              variant: '',
+              quantity: 1,
+              coupon: '',
+            },
+            {
+              name: 'Cosmic Crisp Apple',
+              id: '668ebb86-60b5-451e-92d3-044157d27823',
+              price: '15.55',
+              brand: 'Washington State Apple Farm',
+              category: 'fruit',
+              variant: '',
+              quantity: 1,
+            },
+          ],
+        },
+      },
     });
 
     let [eventName, payload] = expectFS('event');
@@ -148,9 +354,42 @@ describe('Google Tags to FullStory rules', () => {
   it('should read enhanced ecommerce checkout', () => {
     ExpectObserver.getInstance().create({
       rules: [
-        { ...expectRule('fs-ga-e-commerce-checkout-product') },
-        { ...expectRule('fs-ga-e-commerce-checkout-action'), source: 'dataLayer[10]' },
+        expectRule('fs-ga-e-commerce-checkout-product'),
+        expectRule('fs-ga-e-commerce-checkout-action'),
       ],
+    });
+
+    expectGlobal('dataLayer').push({
+      event: 'checkout',
+      ecommerce: {
+        checkout: {
+          actionField: {
+            action: 'checkout',
+            step: 1,
+            option: 'Visa',
+          },
+          products: [
+            {
+              name: 'Heritage Huckleberries',
+              id: 'P000525722',
+              price: '2.99',
+              brand: 'Heritage',
+              category: 'fruit',
+              variant: '',
+              quantity: 1,
+            },
+            {
+              name: 'Cosmic Crisp Apple',
+              id: '668ebb86-60b5-451e-92d3-044157d27823',
+              price: '15.55',
+              brand: 'Washington State Apple Farm',
+              category: 'fruit',
+              variant: '',
+              quantity: 1,
+            },
+          ],
+        },
+      },
     });
 
     let [eventName, payload] = expectFS('event');
@@ -171,9 +410,26 @@ describe('Google Tags to FullStory rules', () => {
   it('should read enhanced ecommerce refund', () => {
     ExpectObserver.getInstance().create({
       rules: [
-        { ...expectRule('fs-ga-e-commerce-refund-product') },
-        { ...expectRule('fs-ga-e-commerce-refund-action'), source: 'dataLayer[12]' },
+        expectRule('fs-ga-e-commerce-refund-product'),
+        expectRule('fs-ga-e-commerce-refund-action'),
       ],
+    });
+
+    expectGlobal('dataLayer').push({
+      ecommerce: {
+        refund: {
+          actionField: {
+            action: 'refund',
+            id: 'T12345',
+          },
+          products: [
+            {
+              id: 'P000525722',
+              quantity: 1,
+            },
+          ],
+        },
+      },
     });
 
     let [eventName, payload] = expectFS('event');
@@ -184,37 +440,5 @@ describe('Google Tags to FullStory rules', () => {
     expectEqual(eventName, 'refund_product');
     expectEqual(payload.id, 'P000525722');
     expectEqual(payload.quantity, 1);
-  });
-
-  it('should set the user', () => {
-    ExpectObserver.getInstance().create({
-      rules: [expectRule('fs-ga-user-vars')],
-    });
-
-    let [eventName, payload] = expectFS('setUserVars');
-    expectEqual(eventName, '101');
-    expectEqual(payload.userType, 'member');
-
-    expectGlobal('dataLayer').push({
-      userProfile: {
-        userId: '201',
-        userType: 'admin',
-        loyaltyProgram: 'early-adopter',
-        hashedEmail: '555-12232-2332232-222',
-      },
-    });
-
-    [eventName, payload] = expectFS('setUserVars');
-    expectEqual(eventName, '201');
-    expectEqual(payload.userType, 'admin');
-  });
-
-  it('should read any event not containing ecommerce and remove event ID', () => {
-    ExpectObserver.getInstance().create({
-      rules: [expectRule('fs-ga-event')],
-    });
-
-    const payload = expectFS('event')[1];
-    expectUndefined(payload, 'ecommerce', 'gtm.uniqueEventId');
   });
 });
