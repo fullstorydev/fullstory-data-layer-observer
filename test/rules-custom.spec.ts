3d2d7cb1973a85209aaa68a52fc8a55518226248
Nate Donze <nate@fullstory.com>
2022-02-07 13:29:14 -0600

    Add test for only triggering rules for property changes matching rule selector

---
+++ test/rules-custom.spec.ts
@@ -0,0 +1,61 @@
+import 'mocha';
+
+import { DataLayerRule } from '../src/observer';
+import {
+  expectEqual, waitForFS, setupGlobals, ExpectObserver, expectGlobal,
+} from './utils/mocha';
+
+describe('Custom rules', () => {
+  afterEach(() => {
+    ExpectObserver.getInstance().cleanup();
+  });
+
+  it('should only trigger rules for property changes that match rule selector', async () => {
+    const s = {
+      eVar11: 'test',
+      eVar22: 'test',
+    };
+
+    setupGlobals(([
+      ['s', s],
+    ]));
+
+    const rules: DataLayerRule[] = [
+      {
+        source: 's[^(eVar1)]',
+        operators: [
+          {
+            name: 'insert',
+            value: 'eVar1',
+          },
+        ],
+        destination: 'FS.event',
+      },
+      {
+        source: 's[^(eVar2)]',
+        operators: [
+          {
+            name: 'insert',
+            value: 'eVar2',
+          },
+        ],
+        destination: 'FS.event',
+      },
+    ];
+
+    ExpectObserver.getInstance().create({ rules });
+
+    expectGlobal('s').eVar11 = 11;
+
+    // Wait for event dispatch to result in rule destination calls
+    let [eventName, payload] = await waitForFS('event');
+    expectEqual('eVar1', eventName);
+    expectEqual(11, payload.eVar11);
+
+    expectGlobal('s').eVar22 = 22;
+
+    [eventName, payload] = await waitForFS('event');
+    expectEqual('eVar2', eventName);
+    expectEqual(22, payload.eVar22);
+  });
+});
