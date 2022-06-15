d7516d2fab49120ec5d21c9bd21eeaf15cb74dbd
Nate Donze <nate@fullstory.com>
2022-06-15 16:21:57 -0500

    Add CEDDL ruleset tests which are runnable via node or browser

---
+++ test/rules-ceddl-fullstory-hybrid.spec.ts
@@ -0,0 +1,118 @@
+import 'mocha';
+
+import { expectEqual, expectMatch, expectUndefined } from './utils/mocha';
+import { RulesetTestHarness, getRulesetTestEnvironments } from './utils/ruleset-test-harness';
+import { basicDigitalData } from './mocks/CEDDL';
+
+import '../rulesets/ceddl.js';
+
+const ceddlRulesKey = '_dlo_rules_ceddl';
+const ceddlRules = (window as Record<string, any>)[ceddlRulesKey];
+
+describe('CEDDL hybrid node and browser tests', () => {
+  getRulesetTestEnvironments().forEach((testEnv) => {
+    describe(`test environment: ${testEnv.name}`, () => {
+      let testHarness: RulesetTestHarness;
+
+      beforeEach(async () => {
+        testHarness = await testEnv.createTestHarness(ceddlRules, { digitalData: basicDigitalData });
+      });
+
+      afterEach(async () => {
+        await testHarness.tearDown();
+      });
+
+      it('sends the first CEDDL product to FS.event', async () => {
+        await testHarness.execute(() => {
+          ((globalThis || window) as any).digitalData.product[0].attributes = { customProp: 'foo' };
+        });
+
+        const [eventName, payload] = await testHarness.popEvent();
+        expectEqual(eventName, 'product');
+        expectMatch(payload, basicDigitalData.product[0].productInfo, 'sku', 'productID', 'productName');
+        expectMatch(payload, basicDigitalData.product[0].category, 'primaryCategory');
+        expectEqual(payload.customProp, 'foo');
+      });
+
+      it('sends CEDDL cart to FS.event', async () => {
+        await testHarness.execute(() => {
+          ((globalThis || window) as any).digitalData.cart.attributes = { promotion: 'LaborDay2020' };
+        });
+
+        const [eventName, payload] = await testHarness.popEvent();
+        expectEqual(eventName, 'cart');
+        expectEqual(payload.cartID, basicDigitalData.cart.cartID);
+        expectEqual(payload.basePrice, basicDigitalData.cart.price.basePrice);
+        expectEqual(payload.promotion, 'LaborDay2020');
+      });
+
+      it('sends dynamic CEDDL cart item additions to FS.event', async () => {
+        const firstProduct = basicDigitalData.product[0];
+
+        const secondProduct = {
+          ...firstProduct,
+          productInfo: {
+            ...firstProduct.productInfo,
+            sku: 'test',
+          },
+        };
+
+        await testHarness.execute((args) => {
+          ((globalThis || window) as any).digitalData.cart.item.push(args![0]);
+        }, [firstProduct]);
+
+        let [eventName, payload] = await testHarness.popEvent();
+        expectEqual(eventName, 'cart_item');
+        expectEqual(payload.productInfo.sku, firstProduct.productInfo.sku);
+        expectUndefined(payload, 'linkedProduct');
+
+        await testHarness.execute((args) => {
+          ((globalThis || window) as any).digitalData.cart.item.push(args![0]);
+        }, [secondProduct]);
+
+        [eventName, payload] = await testHarness.popEvent();
+        expectEqual(eventName, 'cart_item');
+        expectEqual(payload.productInfo.sku, secondProduct.productInfo.sku);
+        expectUndefined(payload, 'linkedProduct');
+      });
+
+      it('does not send CEDDL cart item products to FS.event on load', (done) => {
+        // digitalData.cart.item already has an item. Here, we're verifying no FS.event
+        // calls were made as would be the case with readOnLoad: true.
+        testHarness.popEvent(500)
+          .then(() => {
+            throw new Error('Expected rejected promise.');
+          })
+          .catch(() => {
+            done();
+          });
+      });
+
+      it('sends CEDDL page properties to FS.event', async () => {
+        await testHarness.execute(() => {
+          ((globalThis || window) as any).digitalData.page.attributes = { framework: 'react' };
+        });
+
+        const [eventName, payload] = await testHarness.popEvent();
+        expectEqual(eventName, 'page');
+
+        // NOTE these are flattened but you could also simply send digitalData.page
+        expectMatch(payload, basicDigitalData.page.pageInfo,
+          'pageID', 'pageName', 'sysEnv', 'variant', 'breadcrumbs', 'author', 'language', 'industryCodes', 'publisher');
+
+        expectEqual(payload.primaryCategory, basicDigitalData.page.category.primaryCategory);
+        expectEqual(payload.framework, 'react'); // verify custom property
+        expectEqual(payload.version, basicDigitalData.page.pageInfo.version);
+
+        // check converted values
+        expectEqual(payload.issueDate.toString(), new Date(basicDigitalData.page.pageInfo.issueDate).toString());
+        expectEqual(payload.effectiveDate.toString(),
+          new Date(basicDigitalData.page.pageInfo.effectiveDate).toString());
+        expectEqual(payload.expiryDate.toString(), new Date(basicDigitalData.page.pageInfo.expiryDate).toString());
+
+        // NOTE we have other ways in FullStory to see these
+        expectUndefined(payload, 'destinationURL', 'referringURL');
+      });
+    });
+  });
+});
---
+++ test/utils/ruleset-test-harness.ts
@@ -0,0 +1,133 @@
+/* eslint-disable class-methods-use-this */
+/* eslint-disable no-underscore-dangle */
+import 'mocha';
+import {
+  BrowserType, Browser, Page, chromium, firefox, webkit,
+} from 'playwright';
+
+import {
+  expectFS, ExpectObserver, setupGlobals, expectGlobal,
+} from './mocha';
+
+export interface RulesetTestHarness {
+  setUp: (rules: any, dataLayer: any) => Promise<void>;
+  tearDown: () => Promise<void>;
+  execute: (action: (args?: any[]) => void, args?: any[]) => Promise<void>;
+  popEvent: (timeoutMs?: number) => Promise<any[]>;
+}
+
+interface RulesetTestEnvironment {
+  name: string;
+  createTestHarness: (rules: any, dataLayer: any) => Promise<RulesetTestHarness>;
+}
+
+const dloScriptSrc = process.env.PLAYWRIGHT_DLO_SCRIPT_SRC;
+
+const nodeTestHarness: RulesetTestHarness = {
+  setUp: (rules: any, dataLayer: any) => {
+    setupGlobals(
+      Object.keys(dataLayer).map((key) => [key, dataLayer[key]]),
+    );
+    ExpectObserver.getInstance().create({ rules });
+    return Promise.resolve();
+  },
+
+  tearDown: () => {
+    ExpectObserver.getInstance().cleanup();
+    return Promise.resolve();
+  },
+
+  execute: (action: (args?: any) => void, args?: any) => {
+    action(args);
+    return Promise.resolve();
+  },
+
+  popEvent: async (timeoutMs: number = 1000) => new Promise<any[]>((resolve, reject) => {
+    const startTime = performance.now();
+
+    const checkForEvents = setInterval(() => {
+      const currentTime = performance.now();
+      if (currentTime - startTime >= timeoutMs) {
+        clearInterval(checkForEvents);
+        reject(new Error(`Timeout ${timeoutMs} exceeded waiting for FS.event call.`));
+      }
+
+      if (expectGlobal('FS').callQueues.event.length) {
+        clearInterval(checkForEvents);
+        resolve(expectFS('event'));
+      }
+    }, 50);
+  }),
+};
+
+class BrowserTestHarness implements RulesetTestHarness {
+  private browser: Browser = null!;
+
+  private page: Page = null!;
+
+  constructor(private readonly browserType: BrowserType<{}>) {
+    // Empty
+  }
+
+  async setUp(rules: any, dataLayer: any) {
+    this.browser = await this.browserType.launch();
+    this.page = await this.browser.newPage();
+
+    await this.page.evaluate(([localRules, localDataLayer, localDloScriptSrc]) => {
+      (window as any)._dlo_rules = localRules;
+
+      Object.keys(localDataLayer).forEach((key) => {
+        (window as any)[key] = localDataLayer[key];
+      });
+
+      (window as any).events = [];
+
+      (window as any).FS = {
+        event: (...args: any) => {
+          (window as any).events.push(args);
+        },
+      };
+
+      const dloScriptTag = document.createElement('script');
+      dloScriptTag.src = localDloScriptSrc;
+      document.body.appendChild(dloScriptTag);
+    }, [rules, dataLayer, dloScriptSrc]);
+
+    // Wait for DLO to initialize
+    await this.page.waitForFunction(() => (window as any)._dlo_observer, undefined, { timeout: 1000 });
+  }
+
+  async tearDown() {
+    await this.browser.close();
+  }
+
+  async execute(action: (args?: any[]) => void, args?: any[]) {
+    await this.page.evaluate(action, args);
+  }
+
+  async popEvent(timeoutMs: number = 1000) {
+    await this.page.waitForFunction(() => (window as any).events.length, undefined, { timeout: timeoutMs });
+    return this.page.evaluate(() => (window as any).events.pop());
+  }
+}
+
+export const getRulesetTestEnvironments = (): RulesetTestEnvironment[] => {
+  if (dloScriptSrc) {
+    return [chromium, firefox, webkit].map((browserType) => ({
+      name: browserType.name(),
+      createTestHarness: async (rules: any, dataLayer: any) => {
+        const testHarness = new BrowserTestHarness(browserType);
+        await testHarness.setUp(rules, dataLayer);
+        return testHarness;
+      },
+    }));
+  }
+  return [{
+    name: 'node',
+    createTestHarness: async (rules: any, dataLayer: any) => {
+      const testHarness = nodeTestHarness;
+      await testHarness.setUp(rules, dataLayer);
+      return testHarness;
+    },
+  }];
+};
