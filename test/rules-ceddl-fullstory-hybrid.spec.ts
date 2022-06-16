26d7e119d9d42dbd896c6fd0983adced426e78c9
Nate Donze <nate@fullstory.com>
2022-06-16 08:08:17 -0500

    Clean up rejected promise test logic and comment

--- test/rules-ceddl-fullstory-hybrid.spec.ts
+++ test/rules-ceddl-fullstory-hybrid.spec.ts
@@ -80,16 +80,18 @@ describe('CEDDL hybrid node and browser tests', () => {
         expectUndefined(payload, 'linkedProduct');
       });
 
-      it('does not send CEDDL cart item products to FS.event on load', (done) => {
+      it('does not send CEDDL cart item products to FS.event on load', async () => {
         // digitalData.cart.item already has an item. Here, we're verifying no FS.event
-        // calls were made as would be the case with readOnLoad: true.
-        testHarness.popEvent(500)
-          .then(() => {
-            throw new Error('Expected rejected promise.');
-          })
-          .catch(() => {
-            done();
-          });
+        // calls were made as would occur if readOnLoad were true
+        await new Promise<void>((resolve, reject) => {
+          testHarness.popEvent(500)
+            .then(() => {
+              reject(new Error('Expected rejected promise due to no FS.event calls being present.'));
+            })
+            .catch(() => {
+              resolve();
+            });
+        });
       });
 
       it('sends CEDDL page properties to FS.event', async () => {
