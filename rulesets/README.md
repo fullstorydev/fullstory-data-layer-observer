030874368b194ba0d6ea7ce07e72f96fc1fce7f7
Nate Donze <91545182+ndonze-fs@users.noreply.github.com>
2022-06-28 08:06:12 -0500

    Match test rulesets to those served by data layer integrations (#196)

--- rulesets/README.md
+++ rulesets/README.md
@@ -2,6 +2,12 @@
 
 Rulesets provide out of the box rules that are compatible with common data layers and analytics vendors. Where possible, rules follow vendor guidelines and have associated test cases to ensure operation.
 
+## Adobe App Measturement
+
+- [Ruleset](./adobe-app-measurement.js)
+- [Vendor Guidelines](https://experienceleague.adobe.com/docs/analytics/implementation/vars/page-vars/page-variables.html?lang=en)
+- [Tests](../test/rules-adobe-fullstory.spec.ts)
+
 ## Google
 
 ### Event Measurement
@@ -12,7 +18,7 @@ Rulesets provide out of the box rules that are compatible with common data layer
 
 ### Enhanced Ecommerce (UA)
 
-- [Ruleset](./google-ua-enhanced-ecommerce.js)
+- [Ruleset](./google-enhanced-ecommerce.js)
 - [Vendor Guidelines](https://developers.google.com/tag-manager/enhanced-ecommerce)
 - [Tests](../test/rules-google-fullstory.spec.ts)
 
@@ -21,3 +27,9 @@ Rulesets provide out of the box rules that are compatible with common data layer
 - [Ruleset](./ceddl.js)
 - [Vendor Guidelines](https://www.w3.org/2013/12/ceddl-201312.pdf)
 - [Tests](../test/rules-ceddl-fullstory.spec.ts)
+
+## Tealium Retail
+
+- [Ruleset](./tealium-retail.js)
+- [Vendor Guidelines](https://docs.tealium.com/platforms/getting-started-web/data-layer/definitions/retail/)
+- [Tests](../test/rules-tealium-fullstory.spec.ts)
--- test/rules-google-enhanced-ecommerce-fullstory.spec.ts
+++ test/rules-google-enhanced-ecommerce-fullstory.spec.ts
@@ -3,12 +3,9 @@ import 'mocha';
 import { expectEqual } from './utils/mocha';
 import { RulesetTestHarness, getRulesetTestEnvironments } from './utils/ruleset-test-harness';
 
-// TODO(nate): Update this ruleset to match the rules served by FullStory data layer capture integrations.
-// This will require supplying a "convert" beforeDestination operator to convert string currency values
-// to numbers.
-import '../rulesets/google-ua-enhanced-ecommerce.js';
+import '../rulesets/google-enhanced-ecommerce.js';
 
-const ecommerceRulesKey = '_dlo_rules_google_enhanced_ecommerce';
+const ecommerceRulesKey = '_dlo_rules_google_ec';
 const ecommerceRules = (window as Record<string, any>)[ecommerceRulesKey];
 declare global {
   // eslint-disable-next-line no-var, vars-on-top
@@ -68,7 +65,7 @@ describe('Ruleset: Google Analytics Enhanced Ecommerce to FullStory', () => {
         let [eventName, payload] = await testHarness.popEvent();
         expectEqual(eventName, 'detail_product');
         expectEqual(payload.id, 'P000525722');
-        expectEqual(payload.price, 2.99);
+        expectEqual(payload.price, '2.99');
 
         [eventName, payload] = await testHarness.popEvent();
         expectEqual(eventName, 'detail');
@@ -101,7 +98,7 @@ describe('Ruleset: Google Analytics Enhanced Ecommerce to FullStory', () => {
         let [eventName, payload] = await testHarness.popEvent();
         expectEqual(eventName, 'click_product');
         expectEqual(payload.id, 'P000525722');
-        expectEqual(payload.price, 2.99);
+        expectEqual(payload.price, '2.99');
 
         [eventName, payload] = await testHarness.popEvent();
         expectEqual(eventName, 'click');
@@ -135,7 +132,7 @@ describe('Ruleset: Google Analytics Enhanced Ecommerce to FullStory', () => {
         let [eventName, payload] = await testHarness.popEvent();
         expectEqual(eventName, 'add_product');
         expectEqual(payload.id, 'P000525722');
-        expectEqual(payload.price, 2.99);
+        expectEqual(payload.price, '2.99');
 
         [eventName, payload] = await testHarness.popEvent();
         expectEqual(eventName, 'add');
@@ -168,7 +165,7 @@ describe('Ruleset: Google Analytics Enhanced Ecommerce to FullStory', () => {
         let [eventName, payload] = await testHarness.popEvent();
         expectEqual(eventName, 'remove_product');
         expectEqual(payload.id, 'P000525722');
-        expectEqual(payload.price, 2.99);
+        expectEqual(payload.price, '2.99');
 
         [eventName, payload] = await testHarness.popEvent();
         expectEqual(eventName, 'remove');
@@ -246,16 +243,16 @@ describe('Ruleset: Google Analytics Enhanced Ecommerce to FullStory', () => {
         let [eventName, payload] = await testHarness.popEvent();
         expectEqual(eventName, 'purchase_product');
         expectEqual(payload.id, '668ebb86-60b5-451e-92d3-044157d27823');
-        expectEqual(payload.price, 15.55);
+        expectEqual(payload.price, '15.55');
 
         [eventName, payload] = await testHarness.popEvent();
         expectEqual(eventName, 'purchase_product');
         expectEqual(payload.id, 'P000525722');
-        expectEqual(payload.price, 2.99);
+        expectEqual(payload.price, '2.99');
 
         [eventName, payload] = await testHarness.popEvent();
         expectEqual(eventName, 'purchase');
-        expectEqual(payload.shipping, 5.99);
+        expectEqual(payload.shipping, '5.99');
       });
 
       it('reads enhanced ecommerce checkout', async () => {
@@ -297,12 +294,12 @@ describe('Ruleset: Google Analytics Enhanced Ecommerce to FullStory', () => {
         let [eventName, payload] = await testHarness.popEvent();
         expectEqual(eventName, 'checkout_product');
         expectEqual(payload.id, '668ebb86-60b5-451e-92d3-044157d27823');
-        expectEqual(payload.price, 15.55);
+        expectEqual(payload.price, '15.55');
 
         [eventName, payload] = await testHarness.popEvent();
         expectEqual(eventName, 'checkout_product');
         expectEqual(payload.id, 'P000525722');
-        expectEqual(payload.price, 2.99);
+        expectEqual(payload.price, '2.99');
 
         [eventName, payload] = await testHarness.popEvent();
         expectEqual(eventName, 'checkout');
