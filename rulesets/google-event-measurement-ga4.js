f5b0b1b4cdd72cbabd519cf70d6e5efa86e1bc63
Nate Donze <nate@fullstory.com>
2023-09-18 08:47:34 -0500

    Add Google Event Measurement GA4 ruleset

---
+++ rulesets/google-event-measurement-ga4.js
@@ -0,0 +1,17 @@
+window["_dlo_rules_google_em_ga4"] = [
+  {
+    "id": "fs-gtg-event",
+    "source": "dataLayer",
+    "operators": [
+      { "name": "query", "select": "$[(0,1,2)]" },
+      { "name": "query", "select": "$[?(0=event)]" },
+      { "name": "flatten" },
+      { "name": "rename", "properties": { "0": "gtgCommand", "1": "gtgAction" } },
+      { "name": "query", "select": "$[?(gtgCommand!^gtm)]" },
+      { "name": "query", "select": "$[?(gtgCommand!=optimize.domChange)]" },
+      { "name": "query", "select": "$[?(items=undefined)]" },
+      { "name": "insert", "select": "gtgAction" }
+    ],
+    "destination": "FS.event"
+  }
+];
