9e00d8e13103d5533b2f001881d359b62809c66f
Nate Donze <nate@fullstory.com>
2023-09-19 12:13:32 -0500

    Add GA4 ecommerce select_item tests

---
+++ test/rules-google-enhanced-ecommerce-ga4-fullstory.spec.ts
@@ -0,0 +1,100 @@
+import 'mocha';
+
+import { expect } from 'chai';
+import { expectEqual } from './utils/mocha';
+import {
+  RulesetTestHarness,
+  getRulesetTestEnvironments,
+} from './utils/ruleset-test-harness';
+
+import '../rulesets/google-enhanced-ecommerce-ga4.js';
+
+const ecommerceRulesKey = '_dlo_rules_google_ec_ga4';
+const ecommerceRules = (window as Record<string, any>)[ecommerceRulesKey];
+declare global {
+  // eslint-disable-next-line no-var, vars-on-top
+  var dataLayer: any[];
+}
+
+describe('Ruleset: Google Analytics Enhanced Ecommerce (GA4) to FullStory', () => {
+  getRulesetTestEnvironments().forEach((testEnv) => {
+    describe(`test environment: ${testEnv.name}`, () => {
+      let testHarness: RulesetTestHarness;
+
+      beforeEach(async () => {
+        testHarness = await testEnv.createTestHarness(ecommerceRules, {
+          dataLayer: [],
+        });
+      });
+
+      afterEach(async () => {
+        await testHarness.tearDown();
+      });
+
+      after(async () => {
+        await testEnv.tearDown();
+      });
+
+      it.only('reads enhanced ecommerce select_item GTM event', async () => {
+        await testHarness.execute(() => {
+          globalThis.dataLayer.push({
+            event: 'select_item',
+            ecommerce: {
+              items: [
+                {
+                  item_id: 'sku_123',
+                  item_name: 'first item',
+                  price: '1.23',
+                },
+                {
+                  item_id: 'sku_456',
+                  item_name: 'second item',
+                  price: '4.56',
+                },
+              ],
+              currency: 'USD',
+            },
+          });
+        });
+
+        const [eventName, eventProps] = await testHarness.popEvent();
+        expectEqual(eventName, 'select_item');
+        expectEqual(eventProps.item_id, 'sku_123');
+        expectEqual(eventProps.item_name, 'first item');
+        expectEqual(eventProps.price, 1.23);
+        expect(eventProps.currency).to.be.undefined;
+      });
+
+      it.only('reads enhanced ecommerce select_item gtag event', async () => {
+        await testHarness.execute(() => {
+          globalThis.dataLayer.push([
+            'event',
+            'select_item',
+            {
+              items: [
+                {
+                  item_id: 'sku_123',
+                  item_name: 'first item',
+                  price: '1.23',
+                },
+                {
+                  item_id: 'sku_456',
+                  item_name: 'second item',
+                  price: '4.56',
+                },
+              ],
+              currency: 'USD',
+            }
+          ]);
+        });
+
+        const [eventName, eventProps] = await testHarness.popEvent();
+        expectEqual(eventName, 'select_item');
+        expectEqual(eventProps.item_id, 'sku_123');
+        expectEqual(eventProps.item_name, 'first item');
+        expectEqual(eventProps.price, 1.23);
+        expect(eventProps.currency).to.be.undefined;
+      });
+    });
+  });
+});
