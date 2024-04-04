f25ad9869b9b951870beb090846b52ab92588c5b
Scott Norvell <scottlnorvell@gmail.com>
2024-04-04 11:36:44 -0400

    fix some types

--- src/typeAugmentations/allow.ts
+++ src/typeAugmentations/allow.ts
@@ -23,8 +23,5 @@ declare global {
   interface MapConstructor {
     new (): Map<any, any>;
     new <K, V>(entries?: readonly (readonly [K, V])[] | null): Map<K, V>;
-    readonly prototype: Map<any, any>;
   }
-
-  let Map: MapConstructor;
 }
