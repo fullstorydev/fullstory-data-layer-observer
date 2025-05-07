a8025c5140070e2a55b26a78c0249ae04be2b16a
Irv Salisbury <irv@fullstory.com>
2025-05-06 23:22:03 -0400

    Update for reworked Data Layer Target split out before adding JSON

--- test/operator-domSource.spec.ts
+++ test/operator-domSource.spec.ts
@@ -2,39 +2,24 @@ import { expect } from 'chai';
 import 'mocha';
 
 import { JSDOM } from 'jsdom';
-import deepcopy from 'deepcopy';
 import {
-  DataLayerObserver, FS_API_CONSTANTS, LogMessage, LogMessageType,
+  ConsoleAppender,
+  DataLayerObserver, FS_API_CONSTANTS, Logger, LogMessage, LogMessageType,
 } from '../src';
 import Console from './mocks/console';
-import { expectNoCalls, ExpectObserver, expectParams } from './utils/mocha';
-import FullStory from './mocks/fullstory-recording';
-import { basicDigitalData, CEDDL } from './mocks/CEDDL';
+import { expectNoCalls, expectParams } from './utils/mocha';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const originalConsole = globalThis.console;
-
-interface GlobalMock {
-  dataLayer: any[],
-  digitalData: CEDDL,
-  FS: FullStory
-  console: Console,
-}
-let globalMock: GlobalMock;
+const mockConsole = new Console();
 
 describe.only('domSource unit tests', () => {
   beforeEach(() => {
-    (globalThis as any).dataLayer = [];
-    (globalThis as any).digitalData = deepcopy(basicDigitalData); // NOTE copy so mutations don't pollute tests
-    (globalThis as any).console = new Console();
-    (globalThis as any).FS = new FullStory();
-    globalMock = globalThis as any;
+    Logger.getInstance().appender = new ConsoleAppender();
+    (globalThis as any).console = mockConsole;
   });
 
   afterEach(() => {
-    delete (globalThis as any).dataLayer;
-    delete (globalThis as any).digitalData;
-    delete (globalThis as any).FS;
     (globalThis as any).console = originalConsole;
   });
 
@@ -42,7 +27,7 @@ describe.only('domSource unit tests', () => {
     const observer = new DataLayerObserver();
     // no fsApi or destination
     observer.registerRule({ destination: 'foo' });
-    const [error] = expectParams(globalMock.console, 'error');
+    const [error] = expectParams(mockConsole, 'error');
     expect(error).to.eq(`${LogMessageType.RuleInvalid} ${JSON.stringify({ reason: LogMessage.MissingSource })}`);
   });
 
@@ -51,7 +36,7 @@ describe.only('domSource unit tests', () => {
     // no fsApi or destination
     // @ts-ignore
     observer.registerRule({ source: 'foo', domSource: 'bar', destination: 'test' });
-    const [error] = expectParams(globalMock.console, 'error');
+    const [error] = expectParams(mockConsole, 'error');
     expect(error).to.eq(
       `${LogMessageType.RuleInvalid} ${JSON.stringify({ reason: LogMessage.DuplicateSource })}`,
     );
@@ -60,16 +45,17 @@ describe.only('domSource unit tests', () => {
   it('it should not log error when either source or domSource are present', () => {
     const observer = new DataLayerObserver();
     (globalThis as any).foo = 'test';
-    (globalThis as any).bar = () => globalMock.console.log('Test');
+    (globalThis as any).bar = () => mockConsole.log('Test');
     // no fsApi or destination
     observer.registerRule({ source: 'foo', fsApi: FS_API_CONSTANTS.SET_IDENTITY });
-    expectNoCalls(globalMock.console, 'error');
+    expectNoCalls(mockConsole, 'error');
     observer.registerRule({ domSource: 'foo', destination: 'bar' });
-    expectNoCalls(globalMock.console, 'error');
+    expectNoCalls(mockConsole, 'error');
     delete (globalThis as any).foo;
     delete (globalThis as any).bar;
   });
 
+  // figure out a way to setup DOM and parse
   it('it should not throw an error when selector is not found', () => {
     const dom = new JSDOM(
       `<html>
@@ -81,35 +67,29 @@ describe.only('domSource unit tests', () => {
     (globalThis as any).document = dom.window.document;
     const observer = new DataLayerObserver();
     observer.registerRule({ domSource: 'foo', destination: 'console.log' });
-    expectNoCalls(globalMock.console, 'error');
-    expectNoCalls(globalMock.console, 'log');
+    expectNoCalls(mockConsole, 'error');
+    expectNoCalls(mockConsole, 'log');
   });
 
-  it.only('it should output when selector is found', () => {
+  it('it should output when selector is found', (done) => {
     const dom = new JSDOM(
       `<html>
-       <head>
-         <script type='application/ld+json'>{ "test": "foo" }</script>
-       </head>
-       <body>
-       </body>
-     </html>`,
+         <head>
+           <script type='application/ld+json'>{ "test": "foo" }</script>
+         </head>
+         <body>
+         </body>
+       </html>`,
     );
     (globalThis as any).window = dom.window;
     (globalThis as any).document = dom.window.document;
-    const observer = ExpectObserver.getInstance().create(
-      {
-        readOnLoad: true,
-        rules: [{
-          // source: 'digitalData.page.pageInfo',
-          domSource: '[type="application/ld+json"]',
-          operators: [],
-          destination: 'console.log',
-        },
-        ],
-      },
-    );
-    expectParams(globalMock.console, 'log');
-    ExpectObserver.getInstance().cleanup(observer);
+    const observer = new DataLayerObserver();
+    observer.registerRule({ domSource: '[type="application/ld+json"]', operators: [], destination: 'console.log' });
+    setTimeout(() => {
+      expectNoCalls(mockConsole, 'error');
+      const [log] = expectParams(mockConsole, 'log');
+      expect(log).to.be.deep.eq({ test: 'foo' });
+      done();
+    });
   });
 });
